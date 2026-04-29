a1e92cb945b995b4d44e4289a4b74a74de3b8ee1
itachi-uchia34 <ali00sona1@gmail.com>
2026-04-29 14:20:32 +0500

    Add settings.js with configuration options

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+  SESSION_ID: "",  // add session Id 
+  
+  OWNER_NUMBER: "923110470403", // add owner number 
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
+};
+

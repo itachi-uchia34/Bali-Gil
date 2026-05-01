c2c881dca4e83a18ec4544cb466adc0244898390
itachi-uchia34 <ali00sona1@gmail.com>
2026-05-01 15:56:44 +0500

    Implement WhatsApp connection in index.js
    Set up WhatsApp connection using baileys library.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Deploy From ITACHI-UCHIA repo not this *
+const { makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+
+async function connectWhatsApp() {
+    const { state, saveCreds } = await useMultiFileAuthState('./session');
+    const sock = makeWASocket({ auth: state, printQRInTerminal: true });
+
+    sock.ev.on('creds.update', saveCreds);
+    
+    // Command handler logic goes here (as we built previously)
+    sock.ev.on('messages.upsert', async m => {
+        // ... Handling commands like !menu, !tagall, etc.
+    });
+}
+connectWhatsApp();
+
